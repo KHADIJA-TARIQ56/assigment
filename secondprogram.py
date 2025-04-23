c952786b7e96b4143b1919f2bcc6cd81f0c57883
KHADIJA-TARIQ56 <khadijahtariq26@gmail.com>
2025-04-23 21:19:30 +0500

    Add files via upload

---
+++ secondprogram.py
@@ -0,0 +1,63 @@
+a=2
+b=5
+if(a==b):
+    print("hello")
+elif (a>b):
+    print("yess")
+elif(a<b):
+    print("no")
+else:
+    print("world")
+
+x=56
+y=88
+if(a==b):
+    print("karachi")
+elif (a>b):
+    print("pakistan")
+elif(a<b):
+    print("lahore")
+else:
+    print("multan")
+
+
+a=5
+b=3
+c=2
+if (a>b):
+    if(a>c):
+        print("sajjal")
+    else:
+        print("sameera")
+elif(a<b):
+    if(a<c):
+        print("abdullah")
+    else:
+        print("ahtisham")
+else:
+    print("Noor")
+
+
+x=501
+y=309
+z=289
+if (a<b):
+    if(a<c):
+        print("artificial intelligence")
+    else:
+        print("theory of automata")
+elif(a>b):
+    if(a>c):
+        print("design and analysis of algorithm")
+    else:
+        print("distributed database")
+else:
+    print("data mining")   
+
+
+for x in range(10):
+    print("Palestine")
+    
+    x='p'
+for X in range(6):
+    print(x)
